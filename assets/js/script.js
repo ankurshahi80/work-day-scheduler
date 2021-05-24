3f78a83acb79257eedf72ab1c428e04a3f65beb7
Ankur <ankurshahi@Ankurs-iMac.phub.net.cable.rogers.com>
2021-05-23 20:18:19 -0400

    check the task status every minute

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,42 @@ var formattedDate=moment().format("dddd, MMM Do");
 // assign the formattedDate to id=currentDay
 $("#currentDay").text(formattedDate);
 
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    // if nothting in localStorage, create a new object to track all tasks
+
+    if(!tasks) {
+        tasks
+    }
+}
+// Audit the hour-blocks
+// Get the current time using Moment js
+// Format the time to include the hour only
+var currentTime=moment().format("H");
+currentTime=Math.abs(currentTime);
+
+var auditHours = function() {
+// create an array of all descriptions blocks
+var descBlockArray = $(".description").toArray();
+
+    $.each(descBlockArray,function(){
+        
+        // capture the block id
+        var hourBlockId = $(this).attr("id");
+
+        // find the block hour
+        var hourBlockTime = hourBlockId.replace("-hour-block", "");
+        // compare the block time with the current time to color code the blocks
+        if (currentTime>hourBlockTime){
+            $(this).addClass("past");
+        } else if (currentTime==hourBlockTime) {
+            $(this).addClass("present");
+        } else if(currentTime<hourBlockTime){
+            $(this).addClass("future");
+        }
+        
+    });
+}
+
+// Repeat audit every minute
+setInterval(auditHours(),(1000*60));
